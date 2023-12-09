98e40b84e67729a1653b7efab535cdf90826b4ad
Roman <105315693+mokis3k@users.noreply.github.com>
2023-12-09 19:05:59 +0300

    translating

---
+++ script/calculator.js
@@ -0,0 +1,117 @@
+const calculatorForm = document.querySelector("#calculatorForm");
+const productType = document.querySelector("#productType");
+const calculateButton = document.querySelector("#calculateButton");
+let calculatorProducts = document.querySelector("#calculatorProducts");
+const calculatorProductsWrapper = document.querySelector(
+  "#calculatorProductsWrapper"
+);
+const productExists = document.querySelector("#productExists");
+const productsSelect = document.querySelector("#productsSelect");
+const clearButton = document.querySelector("#clearButton");
+const total = document.querySelector("#total");
+let calculateProductList = [];
+let productsNames = [];
+let productsList = JSON.parse(localStorage.getItem("products"));
+
+calculatorForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  productExists.classList.remove("active");
+
+  let product = new FormData(e.target);
+  product = Object.fromEntries(product.entries());
+  product.amount = +(product.amount.replaceAll(',', '.'));
+
+  if (!calculatorProducts) createCalculatorProducts();
+
+  if (
+    productsNames.includes(product.name) ||
+    !product.name ||
+    !product.amount
+  ) {
+    productExists.classList.add("active");
+  } else {
+    renderProduct(product);
+  }
+
+  e.target.reset();
+});
+
+productsSelect.addEventListener("change", (e) => {
+  addType(e);
+});
+
+const createCalculatorProducts = () => {
+  calculatorProducts = document.createElement("div");
+  calculatorProducts.setAttribute("id", "calculatorProducts");
+  calculatorProducts.setAttribute("class", "calculator__products");
+  calculatorProductsWrapper.append(calculatorProducts);
+};
+
+const renderProduct = (product) => {
+  productsNames.push(product.name);
+  calculateProductList.push(product);
+  const productDiv = document.createElement("div");
+  productDiv.classList.add("calculator__product");
+  const productName = document.createElement("span");
+  productName.innerHTML = product.name;
+  const productAmount = document.createElement("span");
+  productAmount.innerHTML = `${product.amount} ${productType.innerHTML}`;
+  productDiv.append(productName, productAmount);
+  calculatorProducts.append(productDiv);
+};
+
+const addType = (e) => {
+  const productName = e.target.value;
+  productsList.map((obj) => {
+    if (obj.name === productName) {
+      switch (obj.type) {
+        case "штука":
+          productType.innerHTML = "pieces";
+          break;
+        case "літр":
+          productType.innerHTML = "l";
+          break;
+        case "грам":
+          productType.innerHTML = "gr.";
+          break;
+        case "кілограм":
+          productType.innerHTML = "kg";
+          break;
+        case "мілілітр":
+          productType.innerHTML = "ml";
+          break;
+      }
+    }
+  });
+};
+
+const getProductsNames = () => {
+  productsList.map((obj) => {
+    const option = document.createElement("option");
+    option.innerHTML = obj.name;
+    productsSelect.append(option);
+  });
+};
+
+calculateButton.addEventListener("click", (e) => {
+  let totalCost = 0;
+  calculateProductList.map((obj) => {
+    productsList.map((objInner) => {
+      if (obj.name === objInner.name) {
+        totalCost += +obj.amount * +objInner.price;
+      }
+    });
+    total.innerHTML = `${totalCost} UAH`;
+  });
+});
+
+clearButton.addEventListener("click", (e) => {
+  calculatorProducts.innerHTML = "";
+  productsNames = [];
+  calculateProductList = [];
+  total.innerHTML = "00.0 UAH";
+});
+
+if (!productsList) productsList = [];
+getProductsNames();
---
+++ script/products.js
@@ -0,0 +1,93 @@
+const addProductForm = document.querySelector("#addProductForm");
+const productsTable = document.querySelector("#productsTable");
+const productExists = document.querySelector("#productExists");
+const THEADList = ["Ingredient", "Units", "Price (UAH)", "Delete"];
+let productsList = JSON.parse(localStorage.getItem("products"));
+let productsNames = [];
+
+const renderTable = () => {
+  const thead = renderThead();
+  const tbody = renderTbody();
+  tbody.setAttribute("id", "productsTableTbody");
+  productsTable.append(thead, tbody);
+};
+
+const renderThead = () => {
+  const thead = document.createElement("thead");
+  THEADList.map((elem) => {
+    th = document.createElement("th");
+    th.innerHTML = elem;
+    thead.append(th);
+  });
+  return thead;
+};
+
+const renderTbody = () => {
+  const tbody = document.createElement("tbody");
+  productsList.map((product) => {
+    let tr = createTr(product);
+    tbody.append(tr);
+  });
+  return tbody;
+};
+
+const addProduct = (product) => {
+  let tbody = document.querySelector("#productsTableTbody");
+  let tr = createTr(product);
+  tbody.append(tr);
+
+  productsList.push(product);
+  localStorage.setItem("products", JSON.stringify(productsList));
+};
+
+const createTr = (obj) => {
+  productsNames.push(obj.name);
+  let tr = document.createElement("tr");
+  Object.keys(obj).map((key) => {
+    let td = document.createElement("td");
+    if (key === "name") td.setAttribute("id", obj[key]);
+    td.innerHTML = obj[key];
+    tr.append(td);
+  });
+  const tdDelete = document.createElement("td");
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "Delete";
+  deleteButton.addEventListener("click", (e) => deleteButtonHandler(e, obj));
+  tdDelete.append(deleteButton);
+  tr.append(tdDelete);
+
+  return tr;
+};
+
+const deleteButtonHandler = (e, obj) => {
+  parentTr = e.target.closest(`tr`);
+  parentTr.remove();
+  productsList = productsList.filter((product) => product !== obj);
+  productsNames = productsNames.filter(product => product !== obj.name)
+  localStorage.setItem("products", JSON.stringify(productsList));
+};
+
+addProductForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  productExists.classList.remove("active")
+
+  let newProduct = new FormData(e.target);
+  newProduct = Object.fromEntries(newProduct.entries());
+  newProduct.price = +(newProduct.price.replaceAll(',', '.'));
+
+  if (
+    productsNames.includes(newProduct.name) ||
+    !newProduct.name ||
+    !newProduct.type ||
+    !newProduct.price
+  ) {
+    productExists.classList.add("active");
+  } else {
+    addProduct(newProduct);
+  }
+
+  e.target.reset();
+});
+
+if (!productsList) productsList = [];
+renderTable();
